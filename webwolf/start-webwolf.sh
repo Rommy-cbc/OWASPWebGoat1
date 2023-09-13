e96e699fba209eba0da56b79138a20ccdec274b0
Rommy Bhuwaliya <104977830+Rommy-cbc@users.noreply.github.com>
2023-09-13 16:44:19 +0530

    Update start-webwolf.sh

--- webwolf/start-webwolf.sh
+++ webwolf/start-webwolf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Script to start WebWolf, it needs a valid database connection from WebGoat so we wait 8 seconds before starting
-# WebWolf application test
+# WebWolf application test in progress
 
 echo " Waiting for database to be available..."
 sleep 8 && java -Djava.security.egd=file:/dev/./urandom -jar /home/webwolf/webwolf.jar $@
